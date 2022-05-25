430d7f81d7f23719b8b77919654fe8564bb948c8
AndreCypriano23 <andrecypriano23@outlook.com>
2022-05-25 12:16:53 -0300

    Tabela de Filtro Concluída usando Expressões Regulares

--- js/filtra.js
+++ js/filtra.js
@@ -12,8 +12,10 @@ campoFiltro.addEventListener("input", function(){
 			var paciente = pacientes[i];
 			var tdNome = paciente.querySelector(".info-nome");// dentro do .paciente procurei um td que tem a classe info-nome
 			var nome = tdNome.textContent;
+			var expressao = new RegExp(this.value, "i");//expressao regular que vou usar para filtrar cada caractere. No parametro eu quero primeiro buscar o meu this.value e no segundo como que eu quero que ela busque.
 
-				if(nome != this.value){
+
+				if(!expressao.test(nome)){//testar se no meu nome vai ter pelo menos um pedacinho da minha expressão, retorna boolean
 					paciente.classList.add("invisivel");
 				}else{
 					paciente.classList.remove("invisivel");
@@ -21,7 +23,7 @@ campoFiltro.addEventListener("input", function(){
 			}
 
 		}else{
-			
+
 			for(var i = 0; i < pacientes.length; i++){
 				var paciente = pacientes[i];
 				paciente.classList.remove("invisivel"); 
