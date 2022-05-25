77a5989387eb6651b9c8968940aaf4f917f0bbdf
AndreCypriano23 <andrecypriano23@outlook.com>
2022-05-25 11:36:17 -0300

    Tabela de Filtro implementada

--- js/filtra.js
+++ js/filtra.js
@@ -4,4 +4,33 @@ campoFiltro.addEventListener("input", function(){
 	console.log(this.value);//Ou poderia ser: campoFiltro.value;
 
 	//quero só mostrar quem for igual
+	var pacientes = document.querySelectorAll(".paciente");//retorna meus pacientes
+
+		if(this.value.length > 0){
+
+			for(var i = 0; i < pacientes.length; i++){
+			var paciente = pacientes[i];
+			var tdNome = paciente.querySelector(".info-nome");// dentro do .paciente procurei um td que tem a classe info-nome
+			var nome = tdNome.textContent;
+
+				if(nome != this.value){
+					paciente.classList.add("invisivel");
+				}else{
+					paciente.classList.remove("invisivel");
+				}
+			}
+
+		}else{
+			
+			for(var i = 0; i < pacientes.length; i++){
+				var paciente = pacientes[i];
+				paciente.classList.remove("invisivel"); 
+			}
+		}
+
+
+		
+
+
+
 });
